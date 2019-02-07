6f3e9681f7efc9c29ba0be19729bc245952b0a02
League Student <league@iMac-4.attlocal.net>
2019-02-06 16:58:31 -0800

    EvilPolygonsChallenge(IP)

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -34,11 +34,27 @@ r2.sparkle();
 			r2.setPenColor(0, 0, 255);
 		}
 		// 4. Ask the use how many polygons they want to be drawn.
-String num = JOptionPane.showInputDialog(null, "how many polygons do you want batman to draw");
+String num = JOptionPane.showInputDialog(null, "how many polygons do you want batman to draw 100 - 200");
+		int r2r2 = Integer.parseInt(num);
 		// 5. Use the robot to draw the number of polygons the user requested.
+for (int i = 0; i < r2r2; i++) {
+	
 
-		// 6. Make it so your shapes do not overlap
+		for(int ai = 0; ai < 76876786; ai++) {
+r2.move(50);
+r2.turn(50);
+r2.move(51);
+r2.turn(51);
+r2.move(52);
+r2.turn(52);		
+r2.move(53);
+r2.turn(53);		
 		
+		
+		
+		}// 6. Make it so your shapes do not overlap
+}		
 		// 7. Challenge: add more colors to the Option Dialog.
+	
 	}
 }
