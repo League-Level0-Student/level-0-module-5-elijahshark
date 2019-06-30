e9f0fdd1abdaadc8e2a407c6e24bc39085a19573
League Student <league@iMac.attlocal.net>
2019-06-30 11:23:12 -0700

    Fibonacci(ip)

---
+++ src/algorithms/Fibonacci.java
@@ -0,0 +1,53 @@
+package algorithms;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int nn = 1;
+	int n = 0;
+int q; 
+for(int i = 0; i < 12; i++) {
+q = n;
+n = nn;
+nn = q;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
