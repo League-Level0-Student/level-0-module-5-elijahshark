2bd5b3d67407e2f5f578e2d65957175a666005e8
League Student <league@iMac-29.attlocal.net>
2019-07-14 11:32:59 -0700

    FORLOOPGAUNTLET :-1:

---
+++ src/nested_loops/For_Loop_Gauntlet.java
@@ -0,0 +1,14 @@
+package nested_loops;
+
+public class For_Loop_Gauntlet {
+public static void main(String[] args) {
+	
+		
+	for (int i = 2; i <= 100 ; i++) {
+		
+		if (i % 2 == 0) {
+			System.out.println(i);
+		}
+}
+}
+}
